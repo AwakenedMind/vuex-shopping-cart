4992c565a46de94ec6c433ef1696054fbea604de
Robert Arteaga <daspanisharmada@gmail.com>
2020-06-08 04:10:45 -0700

    added shopping cart

---
+++ src/components/ShoppingCart.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Shopping Cart</h1>
+    <ul>
+      <li
+        v-for="product in products"
+        v-bind:key="product.id"
+      >{{product.title}} - {{product.price | currency}} - {{product.quantity}}</li>
+    </ul>
+    <div>Total: {{total | currency}}</div>
+    <button @click="handleCheckout()">Checkout</button>
+
+    <div v-if="this.$store.state.checkoutStatus">{{this.$store.state.checkoutStatus}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    products() {
+      return this.$store.getters.cartProducts;
+    },
+    total() {
+      return this.$store.getters.cartTotal;
+    }
+  },
+  methods: {
+    handleCheckout() {
+      return this.$store.dispatch("checkout");
+    }
+  }
+};
+</script>
+
+<style>
+</style>
