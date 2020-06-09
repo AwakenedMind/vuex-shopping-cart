71bfa6ca0cda79ad324cb75087998c0bda6508c1
Robert Arteaga <daspanisharmada@gmail.com>
2020-06-08 17:05:42 -0700

    shopping cart restructure

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,36 +1,38 @@
 <template>
-  <div>
-    <h1>Shopping Cart</h1>
-    <ul>
-      <li
-        v-for="product in products"
-        v-bind:key="product.id"
-      >{{product.title}} - {{product.price | currency}} - {{product.quantity}}</li>
-    </ul>
-    <div>Total: {{total | currency}}</div>
-    <button @click="handleCheckout()">Checkout</button>
+	<div>
+		<h1>Shopping Cart</h1>
+		<ul>
+			<li v-for="product in products" v-bind:key="product.id">
+				{{ product.title }} - {{ product.price | currency }} -
+				{{ product.quantity }}
+			</li>
+		</ul>
+		<div>Total: {{ total | currency }}</div>
+		<button @click="checkout">Checkout</button>
 
-    <div v-if="this.$store.state.checkoutStatus">{{this.$store.state.checkoutStatus}}</div>
-  </div>
+		<div v-if="checkoutStatus">
+			{{ checkoutStatus }}
+		</div>
+	</div>
 </template>
 
 <script>
+import { mapState, mapGetters, mapActions } from 'vuex';
+
 export default {
-  computed: {
-    products() {
-      return this.$store.getters.cartProducts;
-    },
-    total() {
-      return this.$store.getters.cartTotal;
-    }
-  },
-  methods: {
-    handleCheckout() {
-      return this.$store.dispatch("checkout");
-    }
-  }
+	computed: {
+		...mapGetters('cart', {
+			products: 'cartProducts',
+			total: 'cartTotal',
+		}),
+		...mapState('cart', {
+			checkoutStatus: 'checkoutStatus',
+		}),
+	},
+	methods: {
+		...mapActions('cart', ['checkout']),
+	},
 };
 </script>
 
-<style>
-</style>
+<style></style>
