e70bfd7d024964ba74544f1d858e6e1019c81856
Robert Arteaga <daspanisharmada@gmail.com>
2020-06-08 17:05:34 -0700

    productlis restructure

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,42 +1,52 @@
 <template>
-  <div>
-    <h1>Product List</h1>
-    <img v-if="loading" src="https://i.imgur.com/JfPpwOA.gif" />
-    <ul v-else>
-      <li v-for="product in products" v-bind:key="product.title">
-        <span>{{product.title}} -</span>
-        <span>{{product.price}}</span>
-        <div>Inventory: {{product.inventory}}</div>
-        <button @click="addProductToCart(product)">Add to Cart!</button>
-      </li>
-    </ul>
-  </div>
+	<div>
+		<h1>Product List</h1>
+		<img v-if="loading" src="https://i.imgur.com/JfPpwOA.gif" />
+		<ul v-else>
+			<li v-for="product in products" v-bind:key="product.title">
+				<span>{{ product.title }} -</span>
+				<span>{{ product.price }}</span>
+				<div>Inventory: {{ product.inventory }}</div>
+				<button
+					@click="addProductToCart(product)"
+					:disabled="!productIsInStock(product)"
+				>
+					Add to Cart!
+				</button>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
+import { mapState, mapGetters, mapActions } from 'vuex';
+
 export default {
-  // data is managed through vuex computed object
-  computed: {
-    products() {
-      return this.$store.getters.availableProducts;
-    }
-  },
-  data() {
-    return {
-      loading: false
-    };
-  },
-  methods: {
-    addProductToCart(product) {
-      this.$store.dispatch("addProductToCart", product);
-    }
-  },
-  created() {
-    this.loading = true;
-    this.$store.dispatch("fetchProducts").then(() => (this.loading = false));
-  }
+	// data is managed through vuex computed object
+	computed: {
+		...mapState({ products: (state) => state.products.items }),
+		...mapGetters('products', {
+			productIsInStock: 'productIsInStock',
+		}),
+	},
+
+	data() {
+		return {
+			loading: false,
+			productIndex: 1,
+		};
+	},
+	methods: {
+		...mapActions({
+			fetchProducts: 'products/fetchProducts',
+			addProductToCart: 'cart/addProductToCart',
+		}),
+	},
+	created() {
+		this.loading = true;
+		this.fetchProducts().then(() => (this.loading = false));
+	},
 };
 </script>
 
-<style>
-</style>
+<style></style>
